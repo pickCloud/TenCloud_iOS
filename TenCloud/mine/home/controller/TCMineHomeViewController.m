613ae01ad68b504b59ba7c06bafb682324ff3ed4
dx <dx@ktyx.cn>
2017-12-13 20:52:25 +0800

    实现退出账号功能

--- TenCloud/mine/home/controller/TCMineHomeViewController.m
+++ TenCloud/mine/home/controller/TCMineHomeViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "TCMineHomeViewController.h"
+#import "TCLoginViewController.h"
 
 @interface TCMineHomeViewController ()
-
+- (IBAction) onLogout:(id)sender;
 @end
 
 @implementation TCMineHomeViewController
@@ -25,14 +26,11 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction) onLogout:(id)sender
+{
+    [[TCLocalAccount shared] logout];
+    TCLoginViewController *loginVC = [TCLoginViewController new];
+    [[[UIApplication sharedApplication] keyWindow] setRootViewController:loginVC];
 }
-*/
 
 @end
