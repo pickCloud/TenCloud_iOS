b9248b60eacffa4e1d138b79062b4584a71e78d0
dx <dx@ktyx.cn>
2017-12-26 22:24:38 +0800

    修复问题: 返回按钮位置偏右

--- TenCloud/public/ui/controller/TCViewController.m
+++ TenCloud/public/ui/controller/TCViewController.m
@@ -42,9 +42,16 @@
         
         button.imageEdgeInsets = UIEdgeInsetsZero;
          */
+        CGFloat btnOffset = TCSCALE(8);
+        button.contentEdgeInsets = UIEdgeInsetsMake(0, -btnOffset, 0, 0);
         UIBarButtonItem *backButtonItem = [[UIBarButtonItem alloc] initWithCustomView:button];
         self.navigationItem.leftBarButtonItem = backButtonItem;
-        
+        /*
+        UIBarButtonItem *fixedButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil];
+        fixedButton.width = -15;
+        //self.navigationItem.leftBarButtonItems = @[fixedButton, backButtonItem];
+        //self.navigationItem.leftBarButtonItems = @[backButtonItem, fixedButton];
+        */
         self.navigationController.interactivePopGestureRecognizer.delegate = self;
     }
 }
