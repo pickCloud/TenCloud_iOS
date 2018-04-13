18c7cb9f97c1e4ab1497faf2e2c10b421782647b
dx <dx@ktyx.cn>
2018-04-13 12:46:01 +0800

    网页捕获github登录成功事件，并发送通知

--- TenCloud/app/bindRepo/controller/GithubLoginViewController.m
+++ TenCloud/app/bindRepo/controller/GithubLoginViewController.m
@@ -9,7 +9,7 @@
 #import "GithubLoginViewController.h"
 //#import <WebKit/WebKit.h>
 
-@interface GithubLoginViewController ()
+@interface GithubLoginViewController ()<UIWebViewDelegate>
 @property (nonatomic, weak) IBOutlet    UIWebView   *webView;
 @end
 
@@ -22,7 +22,11 @@
     // Do any additional setup after loading the view from its nib.
     NSURL *eulaURL = [NSURL URLWithString:_loginURLString];
     NSURLRequest *eulaReq = [NSURLRequest requestWithURL:eulaURL];
+    [_webView setDelegate:self];
     [_webView loadRequest:eulaReq];
+    
+    
+    //http://47.75.159.100/?token=true
 }
 
 - (void)didReceiveMemoryWarning {
@@ -40,4 +44,33 @@
 }
 */
 
+#pragma mark - webview delegate
+- (BOOL)webView:(UIWebView *)webView
+shouldStartLoadWithRequest:(NSURLRequest *)request
+ navigationType:(UIWebViewNavigationType)navigationType{
+    //NSString *title = [webView stringByEvaluatingJavaScriptFromString:@"document.title"];
+    //NSLog(@"start load title:%@",title);
+    NSString *urlStr = webView.request.URL.absoluteString;
+    NSString *successAddr = [NSString stringWithFormat:@"%@/?token=true",SERVER_URL_STRING];
+    if ([urlStr hasPrefix:successAddr])
+    {
+        [[NSNotificationCenter defaultCenter] postNotificationName:NOTI_GITHUB_LOGIN_SUCCESS object:nil];
+        [self.navigationController popViewControllerAnimated:YES];
+        return NO;
+    }
+    
+    return YES;
+}
+
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+    //NSLog(@"webViewDidStartLoad = %@",webView.request.URL.absoluteString);
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    //NSLog(@"webViewDidFinishLoad = %@",webView.request.URL.absoluteString);
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error{
+    //NSLog(@"didFailLoadWithError error = %@",error);
+}
 @end
--- TenCloud/app/bindRepo/controller/TCAppBindRepoViewController.m
+++ TenCloud/app/bindRepo/controller/TCAppBindRepoViewController.m
@@ -17,6 +17,7 @@
 @property (nonatomic, strong)   UIButton                *confirmButton;
 @property (nonatomic, strong)   NSMutableArray          *repoArray;
 - (void) onConfirmButton:(UIButton*)button;
+- (void) reloadRepoArray;
 @end
 
 @implementation TCAppBindRepoViewController
@@ -47,65 +48,24 @@
     
     UINib *cellNib = [UINib nibWithNibName:@"TCBindRepoTableViewCell" bundle:nil];
     [self.tableView registerNib:cellNib forCellReuseIdentifier:BIND_REPO_CELL_ID];
-    
-    /*
-    TCGitRepo *repo0 = [TCGitRepo new];
-    repo0.name = @"不绑定";
-    [_repoArray addObject:repo0];
-    TCGitRepo *repo1 = [TCGitRepo new];
-    repo1.name = @"python-2048";
-    repo1.address = @"http://github.com/1234567/python-2048";
-    [_repoArray addObject:repo1];
-    TCGitRepo *repo2 = [TCGitRepo new];
-    repo2.name = @"python-2048";
-    repo2.address = @"http://github.com/1234567/python-2048";
-    [_repoArray addObject:repo2];
-     */
-    
-    /*
-    NSString *urlStr = [NSString stringWithFormat:@"https://github.com/login/oauth/authorize?client_id=%@&state=1&redirect_uri=%@",GITHUB_CLIENT_ID,GITHUB_CALLBACK];
-     */
-    [self startLoading];
-    __weak __typeof(self) weakSelf = self;
-    TCGithubReposRequest *req = [TCGithubReposRequest new];
-    req.url = SERVER_URL_STRING;
-    [req startWithSuccess:^(NSArray<TCAppRepo *> *repoArray) {
-        NSLog(@"get repos amount%ld",repoArray.count);
-        [weakSelf.repoArray addObjectsFromArray:repoArray];
-        [weakSelf.tableView reloadData];
-        [weakSelf stopLoading];
-    } failure:^(NSString *message, NSString *urlStr) {
-        NSLog(@"message:%@",message);
-        NSLog(@"urlStr:%@",urlStr);
-        if (urlStr && urlStr.length > 0)
-        {
-            GithubLoginViewController *vc = [GithubLoginViewController new];
-            vc.loginURLString = urlStr;
-            [weakSelf.navigationController pushViewController:vc animated:YES];
-        }else
-        {
-            [MBProgressHUD showError:message toView:nil];
-        }
-        [weakSelf stopLoading];
-    }];
-    
 
+    [self startLoading];
+    [self reloadRepoArray];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(reloadRepoArray) name:NOTI_GITHUB_LOGIN_SUCCESS object:nil];
+
+}
+
+- (void) dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 #pragma mark - Table view data source
 
@@ -144,5 +104,28 @@
         }
     }
     [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void) reloadRepoArray
+{
+    __weak __typeof(self) weakSelf = self;
+    TCGithubReposRequest *req = [TCGithubReposRequest new];
+    req.url = SERVER_URL_STRING;
+    [req startWithSuccess:^(NSArray<TCAppRepo *> *repoArray) {
+        [weakSelf.repoArray addObjectsFromArray:repoArray];
+        [weakSelf.tableView reloadData];
+        [weakSelf stopLoading];
+    } failure:^(NSString *message, NSString *urlStr) {
+        if (urlStr && urlStr.length > 0)
+        {
+            GithubLoginViewController *vc = [GithubLoginViewController new];
+            vc.loginURLString = urlStr;
+            [weakSelf.navigationController pushViewController:vc animated:YES];
+        }else
+        {
+            [MBProgressHUD showError:message toView:nil];
+        }
+        [weakSelf stopLoading];
+    }];
 }
 @end
